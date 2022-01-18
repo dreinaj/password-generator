1d6e38bc5d8db8d82a859468db0a9064148e5378
Gabriel Reina <dreinaj@gmail.com>
2022-01-18 15:51:21 -0700

    comments on code

--- script.js
+++ script.js
@@ -1,28 +1,8 @@
 // Assignment code here
+//declaring options which are the options the users will have as they set the criteria for the password
 var options;
 
-// character = ["!", "?", "<", ">", "@", "#", "$", "%", "^", "&", "*"];
-// number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// letters = letters.map(toLowerCase);
-// letters2 = letters.map(toUpperCase);
-
-// var password = [];
-
-// var toUpperCase = function (u) {
-//   return u.toUpperCase();
-// };
-
-// var toLowerCase = function (l) {
-//   return l.toLowerCase();
-// };
-
-// var get = document.querySelector("#generate");
-
-// get.addEventListener("click", function () {
-//   pwd = generatePassword();
-//   document.getElementById("password").placeholder = pwd;
-// });
-
+//delcaring the generate password function with a prompt of the criteria for the number of values
 function generatePassword() {
   var enter = parseInt(prompt("How many characters? Please choose between 8 and 128 characters"));
 
@@ -30,13 +10,13 @@ function generatePassword() {
     return "Please enter a valid number between 8 and 128"
   }
 
-
+//declaration of the value options that the user will have when setting the criteria
   var confirmLowerCase = confirm("Will this password contain Lowercase letters?");
   var confirmUpperCase = confirm("Will this password contain Uppercase letters?");
   var confirmNumber = confirm("Will this password contain numbers?");
   var confirmCharacter = confirm("Will this password contain special characters?");
 
-
+//if user selects FALSE/CANCEL for all of them then we will provide a prompt that they MUST make one selection
   if (!confirmLowerCase && !confirmUpperCase && !confirmNumber && !confirmCharacter) {
     return "Please select atleast one option"
   }
@@ -60,6 +40,7 @@ function generatePassword() {
   }
 console.log (correctValues)
 
+//marth random of values based on the criteria the user selects
   for (var i = 0; i < enter; i++) {
     var pickOptions = correctValues[Math.floor(Math.random() * correctValues.length)];
     password.push(pickOptions);
@@ -79,8 +60,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
